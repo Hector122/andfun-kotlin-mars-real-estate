73788774879331dc5c21f0499f9c46901193a9fc
Hector Castillo <hector@Hectors-MacBook-Pro.local>
2022-12-27 11:09:15 -0400

    connect to internet and parsing the JSON response

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,37 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+//Add a Moshi object
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+//Create a Retrofit builder
+private val retrofit = Retrofit.Builder()
+    //.addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+// Implement interface with @GET properties
+interface MarsApiService {
+    // Returns a Retrofit callback that delivers a String
+    @GET("realestate")
+    fun getAllProperties(): Call<List<MarsProperty>>
+}
+
+// A public Api object that exposes the lazy-initialized Retrofit service
+object MarsApi {
+    val retrofitApi: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,4 +17,17 @@
 
 package com.example.android.marsrealestate.network
 
-class MarsProperty()
+import com.squareup.moshi.Json
+
+//{
+//   "price":450000,
+//   "id":"424905",
+//   "type":"buy",
+//   "img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg"
+//}
+data class MarsProperty(
+    val id: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    val price: Double
+)
